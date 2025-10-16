3cfeaef0d5fbac3dae789f4ea24916b8a974c03e
Michaelgnaho <gnahomiael@gmail.com>
2025-10-16 19:18:23 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,16 @@
+import AboutSection from "@/components/About";
+import HeroSection from "@/components/Hero";
 import Image from "next/image";
+import ServicesSection from "@/components/Services";
+import TestimonialsSection from "@/components/Testimonials";
 
 export default function Home() {
   return (
-    <div
-      className="container text-4xl text-green-700"
-      style={{ padding: "20px" }}
-    >
-      <h1>Welcome to God&apos;s Favour Hospital</h1>
-      <p>
-        Your health is our priority. We provide top-notch medical services with
-        a dedicated team of professionals.
-      </p>
-      <Image
-        src="/hospital.jpg"
-        alt="God's Favour Hospital"
-        width={800}
-        height={400}
-        style={{ borderRadius: "8px", marginTop: "20px" }}
-      />
+    <div>
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <TestimonialsSection />
     </div>
   );
 }
