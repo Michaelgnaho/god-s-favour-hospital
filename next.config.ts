13e3f173d7596b588ba534c6e4a655cd916514bb
Michaelgnaho <gnahomiael@gmail.com>
2025-10-17 13:39:11 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["i.pinimg.com"],
+  },
+  reactStrictMode: true, // Enable strict mode
 };
 
 export default nextConfig;
